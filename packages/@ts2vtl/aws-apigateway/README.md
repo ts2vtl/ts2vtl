c60f6d261078fe656654dead45e7fd22f045efb6
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2021-07-23 20:54:01 +0900

    Add @ts2vtl/aws-apigateway

---
+++ packages/@ts2vtl/aws-apigateway/README.md
@@ -0,0 +1,11 @@
+# @ts2vtl/aws-apigateway
+
+## Installation
+
+```
+npm i @ts2vtl/aws-apigateway
+```
+
+## License
+
+MIT
---
+++ packages/@ts2vtl/aws-apigateway/src/index.ts
@@ -0,0 +1,21 @@
+class Method {
+}
+
+class Integration {
+}
+
+class Context {
+}
+
+class Input {
+}
+
+class Util {
+}
+
+export const $method = new Method();
+export const $integration = new Integration();
+export const $context = new Context();
+export const $input = new Input();
+export const $stageVariables: { [name: string]: string } = {};
+export const $util = new Util();

ad72e1441e7ed597817224c5f98b24d09f59355d
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2021-07-30 18:15:04 +0900

    Define Context

--- packages/@ts2vtl/aws-appsync/src/Context.ts
+++ packages/@ts2vtl/aws-appsync/src/Context.ts
@@ -1,8 +1,32 @@
 import { Error } from './Error';
 
-
 export interface RequestContext {
+  arguments: any;
   args: any;
+  identity: {
+    accountId: string;
+    cognitoIdentityPoolId: string;
+    cognitoIdentityId: string;
+    sourceIp: string[];
+    username: string;
+    userArn: string;
+    cognitoIdentityAuthType: string;
+    cognitoIdentityAuthProvider: string;
+    sub: string;
+    issuer: string;
+    claims: any;
+    defaultAuthStrategy: string;
+  };
+  source: any;
+  stash: any;
+  result: any;
+  prev: {
+    result: any;
+  };
+  request: {
+    headers: any;
+  };
+  info: any;
 }
 
 export interface ResponseContext extends RequestContext {

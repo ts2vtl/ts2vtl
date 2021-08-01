87f011a6ecbf25f247f2d61edd511d557db573d5
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2021-08-01 22:02:35 +0900

    Export types

--- packages/@ts2vtl/aws-appsync/src/index.ts
+++ packages/@ts2vtl/aws-appsync/src/index.ts
@@ -1,6 +1,10 @@
 import { ResponseContext } from './Context';
 import { Util } from './Util';
 
+export { RequestContext, ResponseContext } from "./Context";
+export { Error } from "./Error";
+export { Util } from "./Util";
+
 export const $util = new Util();
 export declare const $context: ResponseContext;
 export declare const $ctx: ResponseContext;

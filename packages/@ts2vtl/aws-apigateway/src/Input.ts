4ab739268d6d7d0f963f6888a7ed1cfab0abb8c5
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2021-10-27 12:12:15 +0900

    Define overloads of $input.params()

--- packages/@ts2vtl/aws-apigateway/src/Input.ts
+++ packages/@ts2vtl/aws-apigateway/src/Input.ts
@@ -1,10 +1,15 @@
 export class Input {
   body: string;
+
   json(path: string): any {
   }
-  params(name?: string): { [name: string]: string; } {
+
+  params(): { [name: string]: string; };
+  params(name: string): string;
+  params(name?: string): { [name: string]: string; } | string {
     return {};
   }
+
   path(name: string): any {
   }
 }
